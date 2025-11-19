215e28c490ea497c35de504c389f4021688f0d5e
Mohamed <mybadr1212@gmail.com>
2025-11-20 01:37:30 +0200

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,18 @@
+public abstract class User {
+    private String fname;
+    private String lname;
+    private String email;
+    private String password;
+    private int ssn;
+
+    public User() {
+    }
+    public User(String fname, String lname, int ssn, String email, String password) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.password = password;
+        this.ssn = ssn;
+    }
+
+}
