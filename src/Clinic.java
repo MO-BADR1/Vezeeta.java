15db2c3ece1bba462d9cd96054e00092784e5069
Mohamed <mybadr1212@gmail.com>
2025-11-20 03:31:10 +0200

    Doctor constructo is done

--- src/Clinic.java
+++ src/Clinic.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
 public class Clinic {
+    private String clinicName;
+    private String address;
+    private String phone;
+    private ArrayList<Doctor> doctors;
+    private ArrayList<Appointment> allAppointments;
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+    }
+    public Doctor findDoctorByName(String name) {
+        for (Doctor doctor : doctors) {
+            if (doctor.getFname().equals(name)) {
+                return doctor;
+            }
+        }
+        return null;
+
+    }
+    public Doctor findDoctorById(int id) {
+    for (Doctor doctor : doctors) {
+        if (doctor.getId() == id) {
+            return doctor;
+        }
+    }
+    return null;
+    }
+    public void  booking(Doctor doctor, Patient patient,Appointment appy) {
+        appy.book(patient.getFname());
+        allAppointments.add(appy);
+    }
+
+
+
+
 
 }
--- src/Doctor.java
+++ src/Doctor.java
@@ -1,52 +1,58 @@
-<<<<<<< HEAD
 import java.util.ArrayList;
-=======
 public class Doctor extends User {
->>>>>>> 79fbb2b6ea850f4b40272c0fadf80b0f21de0ccd
 
-public class Doctor extends User {
-    private int id;
-    private String speciality;
-    private int price;
-    private String prescription;
-    private ArrayList<Appointment> appointments;
-    Clinic c1 = new Clinic();
+        private int id;
+        private String speciality;
+        private int price;
+        private String prescription;
+        private ArrayList<Appointment> appointments;
+        Clinic c1 = new Clinic();
 
-    public void addapointment (Appointment app){
-         appointments.add(app); 
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Doctor(String fname, String lname, int ssn, String email, String password, int id, String speciality, int price, String prescription, ArrayList<Appointment> appointments, Clinic c1) {
+        super(fname, lname, ssn, email, password);
         this.id = id;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public void setSpeciality(String speciality) {
         this.speciality = speciality;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
         this.price = price;
-    }
-
-    public String getPrescription() {
-        return prescription;
-    }
-
-    public void setPrescription(String prescription) {
         this.prescription = prescription;
+        this.appointments = appointments;
+        this.c1 = c1;
     }
-    
-    
-}
+
+    public void addapointment(Appointment app) {
+            appointments.add(app);
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getSpeciality() {
+            return speciality;
+        }
+
+        public void setSpeciality(String speciality) {
+            this.speciality = speciality;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public void setPrice(int price) {
+            this.price = price;
+        }
+
+        public String getPrescription() {
+            return prescription;
+        }
+
+        public void setPrescription(String prescription) {
+            this.prescription = prescription;
+        }
+
+
+    }
