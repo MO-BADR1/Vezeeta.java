208a27570b8b219f31c29401ef3af2ff07c83168
mohamed moneer <mohamedmoneer2005@gmail.com>
2025-11-20 22:32:10 +0200

    finishing the auth manager and adding constructor on the patient

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AuthManager authManager = new AuthManager(sc);
+        ArrayList<Doctor> doctors = new ArrayList<>();
+        ArrayList<Patient> patients = new ArrayList<>();
 
+        authManager.caller(doctors,patients);
     }
 }
